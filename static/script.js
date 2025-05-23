e4922508e99e3e0da5809d1ab742076ee2c278c1
Gustavo Yan <gustavo.goncalves.829@gmail.com>
2025-05-23 12:13:30 -0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+  function openModal() {
+    document.getElementById("modal").style.display = "flex";
+  }
+  
+  function closeModal() {
+    document.getElementById("modal").style.display = "none";
+  }
+  
+  
